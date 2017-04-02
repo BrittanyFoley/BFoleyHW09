2da88958c593dc5f5bdb35ef8e55c2a17b72892e
Brittany Foley <brittanyfoley@brittanys-air.bc.edu>
2017-04-02 18:35:39 -0400

    Commit

---
+++ Weather Gift/DayWeatherCell.swift
@@ -0,0 +1,46 @@
+//
+//  DayWeatherCell.swift
+//  Weather Gift
+//
+//  Created by Brittany Foley on 4/2/17.
+//  Copyright © 2017 Brittany Foley. All rights reserved.
+//
+
+import UIKit
+
+class DayWeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var cellIcon: UIImageView!
+    @IBOutlet weak var cellWeekday: UILabel!
+    @IBOutlet weak var cellMaxTemp: UILabel!
+    @IBOutlet weak var cellMinTemp: UILabel!
+    @IBOutlet weak var cellSummary: UITextView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+ 
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+       
+    }
+    
+    func configureTableCell(dailyForecast: weatherLocation.DailyForecast, timeZone: String) {
+        cellMaxTemp.text = String(format: "%2.f" , dailyForecast.dailyMaxTemp) + "°"
+        cellMinTemp.text = String(format: "%2.f" , dailyForecast.dailyMinTemp) + "°"
+        cellSummary.text = dailyForecast.dailySummary
+        cellIcon.image = UIImage(named: dailyForecast.dailyIcon)
+        let usableDate = Date(timeIntervalSince1970: dailyForecast.dailyDate)
+        let dailyTimeZone = TimeZone(identifier: timeZone)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.timeZone = dailyTimeZone
+        let weekday = dateFormatter.string(from: usableDate)
+        cellWeekday.text = weekday
+        
+    }
+
+}
--- Weather Gift/WeatherLocation.swift
+++ Weather Gift/WeatherLocation.swift
@@ -11,10 +11,23 @@ import Alamofire
 import SwiftyJSON
 
 class weatherLocation {
+    
+    struct DailyForecast {
+        var dailyMaxTemp: Double
+        var dailyMinTemp: Double
+        var dailySummary: String
+        var dailyDate: Double
+        var dailyIcon: String
+    }
+    
     var name = ""
     var coordinates = ""
     var currentTemp = -999999.9
     var dailySummary = ""
+    var currentIcon = ""
+    var currentTime = 0.0
+    var timeZone = ""
+    var dailyForecastArray = [DailyForecast]()
     
     func getWeather(completed: @escaping () -> ()) {
         
@@ -29,13 +42,45 @@ class weatherLocation {
                     print("Temp inside getWeather =\(temperature)")
                     self.currentTemp = temperature
                 } else {
-                    print("Could not return a temperature!") }
+                    print("Could not return a temperature!")
+                }
                    if let summary = json["daily"]["summary"].string {
                     print("Summary inside getWeather =\(summary)")
                     self.dailySummary = summary
                 } else {
+                    print("Could not return a summary!")
+                }
+                if let icon = json["currently"]["icon"].string {
+                    print("ICON inside getWeather =\(icon)")
+                    self.currentIcon = icon
+                } else {
+                    print("Could not return an icon!")}
+                if let time = json["currently"]["time"].double {
+                    print("TIME inside getWeather =\(time)")
+                    self.currentTime = time
+                } else {
+                    print("Could not return a time!")
+                }
+                if let timeZone = json["timezone"].string {
+                    print("TIMEZONE inside getWeather =\(timeZone)")
+                    self.timeZone = timeZone
+                } else {
                     print("Could not return a temperature!")
-                }            case .failure(let error):
+                }
+                let dailyDataArray = json["daily"]["data"]
+                self.dailyForecastArray = []
+                let lastDay = min(dailyDataArray.count-1, 6)
+                for day in 1...lastDay {
+                    let maxTemp = json["daily"]["data"][day]["temperatureMax"].doubleValue
+                    let minTemp = json["daily"]["data"][day]["temperatureMin"].doubleValue
+                    let dailySummary = json["daily"]["data"][day]["summary"].stringValue
+                    let dateValue = json["daily"]["data"][day]["time"].doubleValue
+                    let icon = json["daily"]["data"][day]["icon"].stringValue
+                    let iconName = icon.replacingOccurrences(of: "night", with: "day")
+                    self.dailyForecastArray.append(DailyForecast(dailyMaxTemp: maxTemp, dailyMinTemp: minTemp, dailySummary: dailySummary, dailyDate: dateValue, dailyIcon: iconName))
+                    print("Daily Forecast = \(self.dailyForecastArray)")
+                }
+            case.failure(let error):
                 print(error)
             }
             completed()
